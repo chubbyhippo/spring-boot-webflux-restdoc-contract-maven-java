e6cf8ede65acafca550b336a4aa15461def96abe
ch <mkmsacc@outlook.com>
2023-01-19 14:11:34 +0700

    Add RestTests

---
+++ producer/src/test/java/com/example/producer/RestTests.java
@@ -0,0 +1,48 @@
+package com.example.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@WebFluxTest
+@AutoConfigureRestDocs
+class RestTests {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void shouldGetPlanets() {
+        webTestClient
+                .get()
+                .uri("/api/planets")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody()
+                .jsonPath("$.[0].name").hasJsonPath()
+                .jsonPath("$.[1].name").hasJsonPath()
+                .consumeWith(WebTestClientRestDocumentation
+                        .document("all"));
+    }
+
+    @Test
+    void shouldAddPlanet() {
+        webTestClient
+                .post()
+                .uri("/api/planets")
+                .body(Mono.just("Jupiter"), String.class)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.name").isEqualTo("Jupiter")
+                .consumeWith(WebTestClientRestDocumentation
+                        .document("all"));
+    }
+}
