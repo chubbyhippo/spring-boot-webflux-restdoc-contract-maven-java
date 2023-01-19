1b9aef9e6caae9a500c38af39c5e8d9a9643f4ed
ch <mkmsacc@outlook.com>
2023-01-19 23:53:33 +0700

    Add PlanetController

--- producer/src/main/java/com/example/producer/PlanetController.java
+++ producer/src/main/java/com/example/producer/PlanetController.java
@@ -1,6 +1,8 @@
 package com.example.producer;
 
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,13 +13,13 @@ import java.util.Random;
 public class PlanetController {
 
     @GetMapping("/planets")
-    public List<Planet> getPlanets() {
-        return Arrays.asList(new Planet(1L, "Saturn"),
+    public Flux<Planet> getPlanets() {
+        return Flux.just(new Planet(1L, "Saturn"),
                 new Planet(2L, "Jupiter"));
     }
 
     @PostMapping("/planets")
-    public Planet createPlanet(@RequestBody String name) {
-        return new Planet(new Random().nextLong(), name);
+    public Mono<Planet> createPlanet(@RequestBody String name) {
+        return Mono.just(new Planet(new Random().nextLong(), name));
     }
 }
