9b2867083db41a099a7085b99e928bc90f41f10f
ch <mkmsacc@outlook.com>
2023-01-20 15:20:29 +0700

    Configure random port

--- consumer/src/test/java/com/example/consumer/PlanetStubTests.java
+++ consumer/src/test/java/com/example/consumer/PlanetStubTests.java
@@ -3,20 +3,24 @@ package com.example.consumer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@AutoConfigureStubRunner(ids = {"com.example:producer:+:8080"},
+@AutoConfigureStubRunner(ids = {"com.example:producer:+"},
         stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 class PlanetStubTests {
+
+    @StubRunnerPort("producer")
+    private Integer port;
 
     @Test
     void shouldGetPlanets() {
 
         WebTestClient webTestClient = WebTestClient.bindToServer()
-                .baseUrl("http://localhost:8080")
+                .baseUrl("http://localhost:" + port)
                 .build();
         webTestClient
                 .get()
