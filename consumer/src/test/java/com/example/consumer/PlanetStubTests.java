a770b2556f859bfb3ae293c1e98326c43a0ec5ba
ch <mkmsacc@outlook.com>
2023-01-20 15:12:03 +0700

    Refactor test

--- consumer/src/test/java/com/example/consumer/PlanetStubTests.java
+++ consumer/src/test/java/com/example/consumer/PlanetStubTests.java
@@ -1,44 +1,23 @@
 package com.example.consumer;
 
-import org.codehaus.groovy.reflection.ParameterTypes;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @AutoConfigureStubRunner(ids = {"com.example:producer:+:8080"},
         stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 class PlanetStubTests {
 
-
-    private WebTestClient webTestClient;
-
-    WebClient webClient = WebClient.builder().build();
-
     @Test
     void shouldGetPlanets() {
-//        Flux<Planet> planetFlux = webClient
-//                .get()
-//                .uri("http://localhost:8080/api/planets")
-//                .retrieve()
-//                .bodyToFlux(Planet.class);
-//
-//        StepVerifier
-//                .create(planetFlux)
-//                .expectNext(new Planet(1L, "Saturn"))
-//                .expectNext(new Planet(2L, "Jupiter"))
-//                .verifyComplete();
-        webTestClient = WebTestClient.bindToServer()
+
+        WebTestClient webTestClient = WebTestClient.bindToServer()
                 .baseUrl("http://localhost:8080")
-                    .build();
+                .build();
         webTestClient
                 .get()
                 .uri("/api/planets")
@@ -50,6 +29,5 @@ class PlanetStubTests {
                 .hasSize(2);
 
     }
-
 
 }
